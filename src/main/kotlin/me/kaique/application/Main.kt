c99c20c0b4ebefdde1b29c87cc476434184b9ab3
kaiqkt <kaique..gomes@bandtec.com.br>
2021-09-15 17:57:51 -0300

    feat: created main function

--- src/main/kotlin/me/kaique/application/Main.kt
+++ src/main/kotlin/me/kaique/application/Main.kt
@@ -1,4 +1,10 @@
 package me.kaique.application
 
+import me.kaique.application.web.AuthorizationEntryPoint
+
 object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        AuthorizationEntryPoint.init()
+    }
 }
